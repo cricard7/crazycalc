be9bc7d0d582c833cacc4ab6590d899e57a93dfc
cricard7 <chad_ricard@hotmail.com>
2019-03-28 20:58:10 -0500

    initial commit

---
+++ src/views/secretstpatricksdaytricking.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="about">
+    <div class="containter-fluid">
+    <h1>Hope you like this calulator. It's the best in the world!</h1>
+
+
+    <div class="row">
+
+      <div class="col-md-6 offset-md-3">
+
+  <div class="card">
+  
+  <div class="card-body">
+    
+
+    <div class="row mb-2">
+      <div class="col-md-12">
+          <div class="card">
+          <div class="card-body">
+            <h1 class="display-1">8 X 5 = ERROR!</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">7</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">8</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">9</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">4</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">5</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">6</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">1</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">2</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">3</button>
+            </div> <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">0</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">=</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">off</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">+</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">-</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">X</button>
+            </div>
+            <div class="col-md-4">
+              <button type="button" class="btn btn-secondary disbled btn-lg btn-block mb-2">&#247;</button>
+            </div>
+
+
+
+      </div> 
+
+      </div> 
+      
+     
+      
+    </div>
+
+   
+
+  </div>
+</div>
+
+      </div>
+
+    </div>
+
+     </div> 
+
+
+  </div>
+</template>
